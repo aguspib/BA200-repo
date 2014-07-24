2a03d21f4271be2fafd47c57bc5f91088156437c
Albert Guillén Alfaro <aguillen@biosystems.es>
2014-07-24 15:53:48 +0200

    RQ00086 - History PATIENT results could be re-sent

--- WorkSession/ResultsDelegate.vb
+++ WorkSession/ResultsDelegate.vb
@@ -4307,6 +4307,7 @@ Namespace Biosystems.Ax00.BL
         '''                              HistPatientID and SampleID were saved as NULL
         '''              AG 24/04/2013 - Fill new fields to be added in new history records: LISRequest, ExternalQC, SpecimenID, AwosID, ESOrderID, 
         '''                              ESPatientID, LISOrderID, LISPatientID, LISTestName, LISSampleType, LISUnits
+        '''              AG 24/07/2014 - RQ00086 v3.1.0 Historic patient results can be re-sent (all fields required has to be save although the result is already sent)
         ''' </remarks>
         Private Function MoveWSOrderTestsToHISTModule(ByVal pDBConnection As SqlClient.SqlConnection, ByVal pResultsDS As ResultsDS, _
                                                       ByVal pHistSTDTestsDS As HisTestSamplesDS, ByVal pHistISETestsDS As HisISETestSamplesDS, _
@@ -4492,9 +4493,13 @@ Namespace Biosystems.Ax00.BL
                                     myHistOrderTestsRow.SetHistPatientIDNull()
                                 End If
 
+                                'AG 24/07/2014 - RQ00086 v3.1.0 Historic patient results can be re-sent. Change condition and always save next fields
                                 'AG 24/04/2013 - new fields to be able upload results to LIS from history (only if ExportStatus <> "SENT")
                                 'LISRequest, ExternalQC, SpecimenID, AwosID, ESOrderID, ESPatientID, LISOrderID, LISPatientID, LISTestName, LISSampleType, LISUnits
-                                If (myResultsRow.SampleClass = "PATIENT") AndAlso ((Not myResultsRow.IsExportStatusNull AndAlso myResultsRow.ExportStatus <> "SENT") OrElse (myResultsRow.IsExportStatusNull)) Then
+                                'If (myResultsRow.SampleClass = "PATIENT") AndAlso ((Not myResultsRow.IsExportStatusNull AndAlso myResultsRow.ExportStatus <> "SENT") OrElse (myResultsRow.IsExportStatusNull)) Then
+                                If (myResultsRow.SampleClass = "PATIENT") Then
+                                    'AG 24/07/2014 - RQ00086
+
                                     'Fields get from OrderTests
                                     If Not myResultsRow.IsLISRequestNull Then myHistOrderTestsRow.LISRequest = myResultsRow.LISRequest Else myHistOrderTestsRow.SetLISRequestNull()
                                     If Not myResultsRow.IsExternalQCNull Then myHistOrderTestsRow.ExternalQC = myResultsRow.ExternalQC Else myHistOrderTestsRow.SetExternalQCNull()
