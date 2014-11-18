0dea8c7cb512c24143293124dbac55603f70519a
Susana Angueira <sangueira@biosystems.es>
2014-11-18 17:09:39 +0100

    BA-2128: Set v3.1.1 to all applications DLLs + Added script for update table tfmwVersions to TaskList and DataIntegrity Unit Test

--- Global/Global/My Project/Settings.Designer.vb
+++ Global/Global/My Project/Settings.Designer.vb
@@ -212,7 +212,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("3.10")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("3.11")>  _
         Public Property DataBaseVersion() As String
             Get
                 Return CType(Me("DataBaseVersion"),String)
