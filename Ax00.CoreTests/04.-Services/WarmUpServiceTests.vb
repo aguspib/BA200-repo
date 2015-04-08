042fd7155cb81af2f791944735c784fb11dd13bb
unknown <Ana Carmona@AuxSoftware1.ginper.local>
2015-04-08 11:05:21 +0200

    Fix tests

---
+++ Ax00.CoreTests/04.-Services/WarmUpServiceTests.vb
@@ -0,0 +1,16 @@
+﻿Imports NUnit.Framework
+
+Imports Biosystems.Ax00.Core.Services
+
+Namespace Biosystems.Ax00.Core.Services.Tests
+    <TestFixture()> Public Class WarmUpServiceTests
+
+        <Test()> Public Sub StartServiceTest()
+            Assert.Fail()
+        End Sub
+    End Class
+
+
+End Namespace
+
+
--- Global.Tests/My Project/Settings.Designer.vb
+++ Global.Tests/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.GlobalTests.My.MySettings
+        Friend ReadOnly Property Settings() As Global.GlobalTests1.My.MySettings
             Get
-                Return Global.GlobalTests.My.MySettings.Default
+                Return Global.GlobalTests1.My.MySettings.Default
             End Get
         End Property
     End Module
