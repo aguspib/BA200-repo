426e27706db05127954807457ffddc3e738ae4e9
Manel Ibáñez <mibanez@biosystems.es>
2015-04-20 16:16:49 +0200

    Updated framework tool distribution

--- InstallFramework/installer.vb
+++ InstallFramework/installer.vb
@@ -1,74 +1,75 @@
 ﻿'Imports System.Net.Mime
 Imports System.Security.Policy
 
-Module installer
+Module Installer
 
-    Const GitRemoteLocation = "http://stash.ginper.local:7990/scm/ba/bax00framework.git"
-    Const GitRemoteLocation2 = "sourcetree://cloneRepo?type=stash&baseWebUrl=http%3A%2F%2Fstash.ginper.local%3A7990&cloneUrl=http%3A%2F%2Fmiba%25C3%25B1ez%40stash.ginper.local%3A7990%2Fscm%2Fba%2Fbax00framework.git&user=miba%C3%B1ez"
+    Const FrameworkLocationInStash = "http://stash.ginper.local:7990/scm/ba/bax00framework.git"
+
     Sub Main()
 
-        ' "C:\Users\manel ibañez\AppData\Local\Atlassian\SourceTree\git_local\cmd"
-
-
         Dim gitFolder = AtlassianGitFolder()
-
-        Dim appFolder = AppDomain.CurrentDomain.BaseDirectory()
+        Const cr = Chr(13) & Chr(10)
+        Console.ForegroundColor = ConsoleColor.White
+        Console.BackgroundColor = ConsoleColor.Red
+        Const niceHeader As String =
+            "                                       " & cr &
+            "     BAx00 framework download tool     " & cr &
+            "                                       "
+        Console.WriteLine(niceHeader)
+        Console.ForegroundColor = ConsoleColor.Gray
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.WriteLine("Framework download tool")
         Dim success = False
         While Not success
-            Dim P As New Process
-            P.StartInfo.FileName = gitFolder & "\git.exe"
-            P.StartInfo.Arguments = "clone --recursive " & GitRemoteLocation
-            P.StartInfo.WindowStyle = ProcessWindowStyle.Maximized
-            P.Start()
-            While P.HasExited = False
-
+            Dim gitProcess As New Process
+            gitProcess.StartInfo.FileName = gitFolder & "\git.exe"
+            gitProcess.StartInfo.Arguments = "clone --recursive " & FrameworkLocationInStash
+            gitProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized
+            gitProcess.Start()
+            Dim t = Now.AddSeconds(1)
+            Console.Write("Waiting for GIT authentication and download.")
+            While gitProcess.HasExited = False
+                If Now > t Then
+                    t = Now.AddSeconds(1)
+                    Console.Write(".")
+                End If
             End While
-            success = P.ExitCode = 0
+            Console.WriteLine("")
+            success = gitProcess.ExitCode = 0
             If Not success Then
-                Console.WriteLine("Error downloading git repository. Press 'Y' to retry")
+                Console.Write("Error downloading git repository. Press 'Y' to retry>")
                 If Console.ReadKey(True).KeyChar <> "y"c Then
                     End
+                Else
+                    Console.WriteLine("")
                 End If
             End If
         End While
+        Console.WriteLine("")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine("Important:")
+        Console.WriteLine("Framework repository has been downloaded into BAx00Framework folder.")
+        Console.WriteLine("Remember to add this repository to SourceTree too")
+        Console.ForegroundColor = ConsoleColor.Gray
 
-        If System.IO.File.Exists(appFolder & "\bax00framework\.gitignore") Then
-
-            Dim file = GetSourceTreeFolder() & "\SourceTree.exe"
-            'Dim command = """" & file & """ -f """ & appFolder & """\bax00framework\ status"
-            Dim repoLocation = System.IO.Path.GetFullPath(appFolder & "\bax00framework\")
-            Dim command = " -f """ & repoLocation & """ status"
-            Dim sheller = """" & file & """ " & command
-            Shell(sheller)
-
-            Console.Write("Press enter to exit")
-        Else
-            Console.Write("Something went wrong...")
-
-        End If
+        Console.WriteLine("")
+        Console.Write("Press ENTER to close this window>")
 
         Console.ReadLine()
+
     End Sub
 
     Private Function AtlassianGitFolder() As String
 
-        Const GitSubfolder = "\Atlassian\SourceTree\git_local\cmd"
+        Const gitSubfolder = "\Atlassian\SourceTree\git_local\cmd"
 
         Dim gitFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & GitSubfolder
 
-        If System.IO.Directory.Exists(gitFolder) = False Then
+        If IO.Directory.Exists(gitFolder) = False Then
             Console.WriteLine("Can't find Attalasian GIT version installed. Prcess aborted")
             End
         End If
-        Return System.IO.Path.GetFullPath(gitFolder)
+        Return IO.Path.GetFullPath(gitFolder)
     End Function
 
-    Private Function GetSourceTreeFolder() As String
-        Dim sourceFolder = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) & "\Atlassian\SourceTree"
-        If System.IO.Directory.Exists(sourceFolder) Then
-            Return sourceFolder
-        Else
-            Return ""
-        End If
-    End Function
 End Module
