2e921c37da9bca8b664c99cf347e3ce24cde6a01
Alberto Jimenez <ajimenez@biosystems.es>
2015-05-15 11:40:50 +0200

    BA-2452 New DAL!!

---
+++ Ax00.DataAccess.vb/vWSExecutionsDAO.vb
@@ -0,0 +1,25 @@
+﻿
+
+Public Class vWSExecutionsDAO
+
+    Public Sub New()
+
+    End Sub
+
+    Public Function GetInfoExecutions() As vWSExecutionsDS
+        Dim result As New vWSExecutionsDS()
+        Dim aux As New vWSExecutionsDSTableAdapters.vWSExecutionsSELECTTableAdapter()
+        aux.Fill(result.vWSExecutionsSELECT)
+
+        Return result
+    End Function
+
+    Public Function GetInfoExecutionByExecutionID(executionID As Integer) As vWSExecutionsDS
+        Dim result As New vWSExecutionsDS()
+        Dim aux As New vWSExecutionsDSTableAdapters.vWSExecutionsSELECTTableAdapter()
+        aux.FillByExecutionID(result.vWSExecutionsSELECT, executionID)
+
+        Return result
+    End Function
+
+End Class
