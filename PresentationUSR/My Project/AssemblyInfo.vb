45e4602678f8284d580696b1b6d17d24601d22a1
Alberto Jimenez <ajimenez@biosystems.es>
2015-06-02 11:32:59 +0200

    BA-2345 Update version to 3.0.8

--- PresentationUSR/My Project/AssemblyInfo.vb
+++ PresentationUSR/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.7")> 
-<Assembly: AssemblyFileVersion("3.0.7")> 
+<Assembly: AssemblyVersion("3.0.8")> 
+<Assembly: AssemblyFileVersion("3.0.8")> 
