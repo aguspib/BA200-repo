7213001a9d4c14ca147aa0c2b53b7d835d270f62
Alberto Jimenez <ajimenez@biosystems.es>
2015-06-02 12:11:05 +0200

    BA-2345 Documenting code

--- Ax00.DataAccess.vb/vWSExecutionsDAO.vb
+++ Ax00.DataAccess.vb/vWSExecutionsDAO.vb
@@ -1,11 +1,24 @@
 ﻿
-
+''' <summary>
+''' Class which encapsulates the callings for database objects
+''' </summary>
+''' <remarks></remarks>
 Public Class vWSExecutionsDAO
 
+    ''' <summary>
+    ''' Standard constructor
+    ''' </summary>
+    ''' <remarks></remarks>
     Public Sub New()
 
     End Sub
 
+    ''' <summary>
+    ''' Retrieves all the information related to the current WSExecution, 
+    ''' in a way that the current context is able to deal with
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Function GetInfoExecutions() As vWSExecutionsDS
         Dim result As New vWSExecutionsDS()
         Dim aux As New vWSExecutionsDSTableAdapters.vWSExecutionsSELECTTableAdapter()
@@ -16,6 +29,13 @@ Public Class vWSExecutionsDAO
         Return result
     End Function
 
+    ''' <summary>
+    ''' Retrieves all the information related to a single execution inside the current
+    ''' WSExecution, in a way that the current context is able to deal with
+    ''' </summary>
+    ''' <param name="executionID">the execution Id to look for</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Function GetInfoExecutionByExecutionID(executionID As Integer) As vWSExecutionsDS
         Dim result As New vWSExecutionsDS()
         Dim aux As New vWSExecutionsDSTableAdapters.vWSExecutionsSELECTTableAdapter()
@@ -26,6 +46,16 @@ Public Class vWSExecutionsDAO
         Return result
     End Function
 
+    ''' <summary>
+    ''' Retrieves the required washing solution, used in a given WRUN instruction
+    ''' If the washing solution is distilled water, it returns 'DISTW'
+    ''' Otherwise, it returns the Washing solution code.
+    ''' </summary>
+    ''' <param name="ExecutionID"></param>
+    ''' <param name="AnalyzerID"></param>
+    ''' <param name="WorkSessionID"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Function GetWashingSolution(ExecutionID As Integer, AnalyzerID As String, WorkSessionID As String) As vWSExecutionsDS
         Dim result As New vWSExecutionsDS()
         Dim aux As New vWSExecutionsDSTableAdapters.WashingSolutionSELECTTableAdapter()
