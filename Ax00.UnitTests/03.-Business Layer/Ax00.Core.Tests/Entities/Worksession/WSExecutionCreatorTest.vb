0cc60d1230fbc8f2bbf1896ebb7c0e4ed0f5b6b8
Alberto Jimenez <ajimenez@biosystems.es>
2015-06-02 11:01:27 +0200

    BA-2345

---
+++ Ax00.UnitTests/03.-Business Layer/Ax00.Core.Tests/Entities/Worksession/WSExecutionCreatorTest.vb
@@ -0,0 +1,15 @@
+﻿Imports Biosystems.Ax00.Core.Entities.WorkSession
+Imports NUnit.Framework
+
+
+Namespace Biosystems.Ax00.Core.Entities.WorkSession.Tests
+
+    <TestFixture()> Public Class WSExecutionCreatorTest
+
+        <Test()> Public Sub CreateWSTest()
+            Assert.AreEqual(1, 1)
+        End Sub
+
+    End Class
+
+End Namespace
