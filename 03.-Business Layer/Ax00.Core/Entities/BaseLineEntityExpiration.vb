e48a4e681b491cd1cb82081160bdd588b51ba044
unknown <Meritxell Rodriguez@informaticaID2.ginper.local>
2015-06-18 12:26:20 +0200

    BA-2601

--- 03.-Business Layer/Ax00.Core/Entities/BaseLineEntityExpiration.vb
+++ 03.-Business Layer/Ax00.Core/Entities/BaseLineEntityExpiration.vb
@@ -1,19 +1,69 @@
 ﻿Imports Biosystems.Ax00.Global
 Imports Biosystems.Ax00.Core.Interfaces
 Imports Biosystems.Ax00.Core.Entities
+Imports Biosystems.Ax00.BL
+Imports Biosystems.Ax00.Types
+Imports System.Globalization
 
 Public Class BaseLineEntityExpiration
     Implements IBaseLineExpiration
 
-    Public Property AnalyzerID As String Implements IBaseLineExpiration.AnalyzerID
+    'Public Property AnalyzerID As String Implements IBaseLineExpiration.AnalyzerID
+    Private _analyzer As IAnalyzerManager
+
+    Sub New(analyzer As IAnalyzerManager)
+        Me._analyzer = analyzer
+    End Sub
+
 
     Public ReadOnly Property IsBlExpired As Boolean Implements IBaseLineExpiration.IsBlExpired
         Get
-
+            IsBlExpired = getBLParameter()
         End Get
     End Property
 
-    
+    Public Function getBLParameter() As Boolean
+        Dim resultData As GlobalDataTO = Nothing
+        Dim myAnalyzerSettingsDs As New AnalyzerSettingsDS
+        Dim myAnalyzerSettingsRow As AnalyzerSettingsDS.tcfgAnalyzerSettingsRow
+        Dim myAnalyzerSettingsDelegate As New AnalyzerSettingsDelegate
+        Dim myDate As String = ""
+        Dim dtStartDate As Date
+
+        Try
+
+            Dim AnalyzerID As String = ""
+            resultData = myAnalyzerSettingsDelegate.GetAnalyzerSetting(Nothing, _
+                                                                       AnalyzerID, _
+                                                                       GlobalEnumerates.AnalyzerSettingsEnum.DYNAMICBLDATETIME.ToString())
+
+            If (Not resultData.HasError AndAlso Not resultData.SetDatos Is Nothing) Then
+                myAnalyzerSettingsDs = DirectCast(resultData.SetDatos, AnalyzerSettingsDS)
+
+                If (myAnalyzerSettingsDs.tcfgAnalyzerSettings.Rows.Count = 1) Then
+                    myDate = myAnalyzerSettingsDs.tcfgAnalyzerSettings.First.CurrentValue
+                End If
+
+                If String.Equals(myDate.Trim, String.Empty) Then
+                    Exit Function
+                Else
+                    dtStartDate = DateTime.Parse(myAnalyzerSettingsDs.tcfgAnalyzerSettings.First.CurrentValue, CultureInfo.InvariantCulture)
+                End If
+            End If
+
+
+
+
+
+        Catch ex As Exception
+            Throw ex
+        Finally
+            myAnalyzerSettingsDs = Nothing
+        End Try
+    End Function
+
+
+
     'Public Sub CreationAlarm(ByRef alarm As AlarmEnumerates.Alarms) Implements IBaseLine.CreationAlarm
     '    Dim myGlobal As New GlobalDataTO
     '    Dim myAlarmList As New List(Of AlarmEnumerates.Alarms)
@@ -41,5 +91,5 @@ Public Class BaseLineEntityExpiration
 
 
 
- 
+
 End Class
