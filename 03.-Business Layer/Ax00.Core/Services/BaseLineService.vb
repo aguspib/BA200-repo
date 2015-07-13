0c33ec2b87e06561d85a85ecf37b78cd2a5cf363
unknown <Meritxell Rodriguez@informaticaID2.ginper.local>
2015-07-13 12:00:01 +0200

    Modification required Integration BaseLineService Test. Successful again!

--- 03.-Business Layer/Ax00.Core/Services/BaseLineService.vb
+++ 03.-Business Layer/Ax00.Core/Services/BaseLineService.vb
@@ -1018,8 +1018,7 @@ Namespace Biosystems.Ax00.Core.Services
         ''' <remarks></remarks>
         Private Sub deleteAlarmBlExpired()
 
-            Dim _analyzerAlarmsManager = New AnalyzerAlarms(AnalyzerManager.GetCurrentAnalyzerManager())
-            _analyzerAlarmsManager.ActionAlarm(False, AlarmEnumerates.Alarms.BASELINE_EXPIRED)
+            AnalyzerAlarmsManager.ActionAlarm(False, AlarmEnumerates.Alarms.BASELINE_EXPIRED)
 
         End Sub
 
